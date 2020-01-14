00d191db2dcdd0422eeed3e335ff88c270a09d3c
Harshita Gupta <harshitagupta@jklu.edu.in>
2020-01-14 14:24:37 +0530

    Gradebook Project

---
+++ src/Gradebook/Book.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Gradebook{
+     public class Book{
+         
+
+        //defining constructor as same name as that of class
+        public Book(string name){
+            grades= new List<double>();
+            this.name=name;
+
+        }
+
+        public void AddGrade(double grade){
+           grades.Add(grade);
+
+       } 
+       public Statistics GetStatistics(){
+
+           var result=new Statistics();
+            result.Average=0.0;
+            result.High=double.MinValue;
+            result.Low=double.MaxValue;
+
+
+            // var grades=new List<double>() {12.7,10.3,4.00,6.11};
+            // grades.Add(56.1);
+
+            // grades.Remove(10.3);
+
+            // Console.WriteLine(grades[0]);
+           
+           
+           foreach( var grade in grades){
+                // highGrade=Math.Max(number,highGrade);
+                // LowestGrade=Math.Min(number,LowestGrade);
+                if(grade> result.High){
+                    result.High=grade;
+                }
+                if(grade< result.Low){
+                    result.Low=grade;
+                }
+                result.Average+=grade;   
+            }
+
+             result.Average/= grades.Count;
+
+            return result;
+             
+       }
+
+       
+       public List<double> grades;   //legally allowed to access the grades class as now it is public.
+    //    private List<double> grades;  
+       private string name;   
+    }
+}
---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book= new Book("Harshita Gradebook");
+            book.AddGrade(86.1);
+            book.AddGrade(85.7);
+            book.grades.Add(101);
+            var stats = book.GetStatistics();    //This will store the result object returned by the method with filed High, Low & Average.
+
+
+            Console.WriteLine($"The result is {stats.Average:N1}");
+            Console.WriteLine($"The maximum is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            
+            // var x = 34.1;
+            // var y= 10.3;
+            // var result=(x+y);
+            // System.Console.WriteLine(result);
+
+            // double[] numbers; //declaration of array of numbers
+            // var numbers=new double[] {12.7,10.3,6.11,4.1};
+        //     var numbers=new [] {12.7,10.3,6.11,4.1};
+
+        //     numbers[0]=12.4;   //won't allow to assign value to unassigned variables
+        //     numbers[1]=10.7;
+        //     numbers[2]=6.11;
+        }
+    }
+}
+
+
+
+            // var result=numbers[0];
+            // result+=numbers[1];
+            // result+=numbers[2];
+             
+
+            // foreach( var number in numbers)
+           
